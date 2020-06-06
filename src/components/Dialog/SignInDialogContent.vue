952957f9fbc4ebebee72cbce3026f2a6a2336313
PierreStephaneVoltaire <psvoltaire96@gmail.com>
2020-06-06 10:47:59 -0400

    added packages for functionalities

--- src/components/Dialog/SignInDialogContent.vue
+++ src/components/Dialog/SignInDialogContent.vue
@@ -1,5 +1,24 @@
 <template>
   <div >
+  <form novalidate class="md-layout" >
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-large-size-100">
+            <md-field>
+              <label>Username</label>
+              <md-input v-model="form.username" ></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-large-size-100">
+            <md-field md-clearable>
+              <label>Password</label>
+              <md-input v-model="form.password" type="password" ></md-input>
+            </md-field>
+          </div>
+        </div>
+        <div class="md-layout-item md-large-size-100">
+          <md-button @click="signin"   class="md-primary"> sign in</md-button>
+        </div>
+      </form>
 
   </div>
 </template>
@@ -10,6 +29,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
   @Component({
   })
 export default class SignInDialogContent extends Vue {
+      form={
+        username: '',
+        password: ''
+      }
+
+  signin=() => {
+    console.log(this.form)
+  }
 }
 </script>
 
